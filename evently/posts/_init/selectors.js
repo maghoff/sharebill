68e970782648d72bc30f9cf506fe78d0a7d5b413
maghoff <none@none>
2010-10-18 23:30:23 +0200

    Added a view of credits-debets per user

--- evently/posts/_init/selectors.js
+++ evently/posts/_init/selectors.js
@@ -60,6 +60,8 @@ function() {
                 "after": function(data) {
                     var sumrow = $("table#posts>tbody>tr#totals");
 
+                    var user_total_credits = {};
+
                     for (row in data.rows) {
                         var r = data.rows[row];
                         var type = r.key[0];
@@ -70,7 +72,27 @@ function() {
 
                         var col = get_column(user);
                         sumrow.find("td." + type).slice(col, col+1).text(value);
+
+                        var credit_value;
+                        if (type == "credits") credit_value = value;
+                        else credit_value = -value;
+                        user_total_credits[user] = (user_total_credits[user] || 0) + credit_value;
                     }
+
+                    var list = $('<tbody id="totals_summary"></tbody>');
+                    for (user in user_total_credits) {
+                        var value = user_total_credits[user];
+                        var cr = $('<td class="credits"></td>');
+                        var dr = $('<td class="debets"></td>');
+                        (value > 0 ? cr : dr).text(Math.abs(value) || "");
+
+                        var row = $('<tr></tr>');
+                        row.append($('<td></td>').text(user));
+                        row.append(cr);
+                        row.append(dr);
+                        list.append(row);
+                    }
+                    $("#totals_summary").replaceWith(list);
                 }
             }
         }
