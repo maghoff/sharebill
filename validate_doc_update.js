c3ab3799e9d16d1921cbbad5da27a26b10becd04
maghoff <none@none>
2010-10-21 21:40:47 +0200

    Stricter validation of new documents. The app actually requires these things to work properly

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,6 +1,22 @@
 function(newDoc, oldDoc, userCtx) {
     if (newDoc._deleted) return;
 
+    var assert_has = function(path) {
+        var node = newDoc;
+        for (x in path) {
+            var term = path[x];
+            if (!node[term]) throw({forbidden: 'Minimal structure is: {"meta":{"description":<string>,"timestamp":<RFC3339-timestamp-in-UTC>},"transaction":{"credits":{},"debets":{}}}'});
+            node = node[term];
+        }
+    };
+    assert_has(["meta", "description"]);
+    assert_has(["meta", "timestamp"]);
+    assert_has(["transaction", "credits"]);
+    assert_has(["transaction", "debets"]);
+
+    var rfc3339 = /\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?Z/;
+    if (!newDoc.meta.timestamp.match(rfc3339)) throw({forbidden: 'meta.timestamp must be RFC3339-formatted and in UTC with timezone specifier Z'});
+
     var totals = {"credits": 0, "debets": 0};
     var types = ["credits", "debets"];
     for (type_n in types) {
@@ -9,12 +25,12 @@ function(newDoc, oldDoc, userCtx) {
             var value = newDoc.transaction[type][name];
             totals[type] += value;
 
-            if (value < 0) throw({forbidden: "All values must be positive. " +
+            if (value < 0) throw({forbidden: "All values must be non-negative. " +
                 "(" + type + ": " + name + " = " + value + ")"});
 
-            /*if (value != Math.floor(value)) {
+            if (value != Math.floor(value)) {
                 throw({forbidden : 'The values in a transaction must be whole numbers'});
-            }*/
+            }
         }
     }
     if (totals.credits != totals.debets) {
