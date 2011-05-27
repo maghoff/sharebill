40424f6cb8762476bfbd8a7602592dd3d3d1b501
Magnus Hoff <maghoff@gmail.com>
2011-05-27 12:47:58 +0200

    Only need to listen to inotify in one place. This also gives better couchapp responsiveness

--- develop.sh
+++ develop.sh
@@ -3,16 +3,15 @@
 trap teardown INT
 
 teardown() {
-	screen -X -S couchapp quit
 	screen -X -S jstestdriver quit
 	exit 0
 }
 
-screen -dmS couchapp couchapp autopush
 screen -dmS jstestdriver jstestdriver --port 4224
 
+sleep 1
 google-chrome http://localhost:4224/ &
 
 ./jsTestDriver-tests/autojstestdriver
 
-teardown
+teardown()
