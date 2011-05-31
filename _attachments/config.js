9bac8e9c1560e934a8d6f1175637a5f795e736ce
Magnus Hoff <maghoff@gmail.com>
2011-05-31 14:36:25 +0200

    Use automatic discovery of db and design document names when possible

--- _attachments/config.js
+++ _attachments/config.js
@@ -1,6 +1,10 @@
 var config = (function() {
-    return {
-        db : 'sharebill',
-        design : 'sharebill'
-    };
+    var opts = {};
+
+    if (document.location.pathname.indexOf("_design") == -1) {
+        opts.db = 'sharebill';
+        opts.design = 'sharebill';
+    }
+
+    return opts;
 }());
