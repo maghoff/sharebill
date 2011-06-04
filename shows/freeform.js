e2dd5d44faa55164af2470e239b9b07b58722af3
maghoff <none@none>
2011-06-04 20:36:48 +0200

    Presenting the data

--- shows/freeform.js
+++ shows/freeform.js
@@ -1,5 +1,17 @@
 function(doc, req) {
 	// !json template.freeform
 	var Mustache = require('vendor/couchapp/lib/mustache');
+
+	var make_dict_array = function(d) {
+		var arr = [];
+		for (var key in d) {
+			arr.push({key: key, value: d[key]});
+		}
+		return arr;
+	};
+
+	doc.transaction.debit_array = make_dict_array(doc.transaction.debets);
+	doc.transaction.credit_array = make_dict_array(doc.transaction.credits);
+
 	return Mustache.to_html(template.freeform, doc);
 }
