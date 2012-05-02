2be3cea18cfe2b5d2e9ff7f335064d7f50a270a3
Magnus Hoff <maghoff@gmail.com>
2012-05-02 23:09:30 +0200

    Got rid of an extra roundtrip caused by instance_config

--- sharebill.js
+++ sharebill.js
@@ -1,7 +1,7 @@
 var sharebill = (function () {
 	var isReady = false;
-	var app = null;
 	var runOnReady = [];
+	var app = null;
 	var instance_config = null;
 
 	function onReady(callback, config) {
@@ -13,31 +13,49 @@ var sharebill = (function () {
 	}
 
 	function ready() {
-		console.log(app);
-		isReady = true;
 		runOnReady.forEach(function (cb) {
 			cb.call(app, app);
 		});
 	}
 
+	function maybeReady() {
+		isReady = (instance_config !== null) && (app !== null);
+		if (isReady) ready();
+	}
+
+	function getdb(opts) {
+		var urlPrefix = opts.urlPrefix || "";
+
+		var index = urlPrefix.split('/').length;
+		var fragments = unescape(document.location.href).split('/');
+		var dbname = opts.db || fragments[index + 2];
+		var dname = opts.design || fragments[index + 4];
+
+		return $.couch.db(dbname);
+	}
+
+	var db = getdb(config);
+
+	db.openDoc("instance_config", {
+		success: function(json) {
+			instance_config = json;
+			maybeReady();
+		},
+		error: function() {
+			console.log("This sharebill instance lacks instance_config -- it should be configured! Falling back on default config");
+
+			instance_config = {
+				currency_formatting: {
+					short: "%(wholepart)d"
+				}
+			};
+			maybeReady();
+		}
+	});
+
 	$.couch.app(function (newApp) {
 		app = newApp;
-		app.db.openDoc("instance_config", {
-			success: function(json) {
-				instance_config = json;
-				ready();
-			},
-			error: function() {
-				console.log("This sharebill instance lacks instance_config -- it should be configured! Falling back on default config");
-
-				instance_config = {
-					currency_formatting: {
-						short: "%(wholepart)d"
-					}
-				};
-				ready();
-			}
-		});
+		maybeReady();
 	}, config);
 
 	function formatCurrencyShort(fraction) {
