04533cd832a681707010a8f7f415d225ef0304f9
"Magnus Hoff" <maghoff@gmail.com>
2015-08-27 14:19:16 +0200

    Avoid using two parts for mixed numbers when one part is sufficient

--- src/toMixedNumber.js
+++ src/toMixedNumber.js
@@ -1,5 +1,9 @@
 function toMixedNumber(num) {
 	var floor = SchemeNumber.fn.floor(num);
 	var rest = SchemeNumber.fn['-'](num, floor);
-	return floor.toString() + " " + rest.toString();
+	var parts = [];
+	if (!SchemeNumber.fn['zero?'](floor)) parts.push(floor.toString());
+	if (!SchemeNumber.fn['zero?'](rest)) parts.push(rest.toString());
+	if (!parts.length) parts.push('0');
+	return parts.join(' ');
 }
